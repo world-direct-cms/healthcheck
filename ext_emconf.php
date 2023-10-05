19b5d290673f57628f1c1a9c106a0d8e9c1f045b
Klaus HÃrmann-Engl <Klaus.Hoermann-Engl@world-direct.at>
2023-10-05 07:51:19 +0200

    task: set compatibility to only v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.9.2',
+    'version' => '0.10.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-12.9.99'
+            'typo3' => '10.4.0-10.4.99'
         ],
         'conflicts' => [],
         'suggests' => [
