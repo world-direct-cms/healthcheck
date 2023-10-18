af4884e1fe9835f267c0d495cdbaa1713c3b072b
Klaus HÃrmann-Engl <Klaus.Hoermann-Engl@world-direct.at>
2023-10-18 11:31:26 +0200

    task: add saml metadata probe

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,6 +5,7 @@ defined('TYPO3') || die('Access denied.');
 // Set probes
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['healthcheck']['probe'][] = \WorldDirect\Healthcheck\Probe\DatabaseProbe::class;
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['healthcheck']['probe'][] = \WorldDirect\Healthcheck\Probe\SchedulerProbe::class;
+$GLOBALS['TYPO3_CONF_VARS']['EXT']['healthcheck']['probe'][] = \WorldDirect\Healthcheck\Probe\SamlMetadataProbe::class;
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['healthcheck']['probe'][] = \WorldDirect\Healthcheck\Probe\CacheProbe::class;
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['healthcheck']['probe'][] = \WorldDirect\Healthcheck\Probe\ExternalImportProbe::class;
 
