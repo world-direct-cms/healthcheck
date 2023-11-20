0d2217d7ed18d15d9e3cd7c8a0ef236c55343385
Klaus HÃrmann-Engl <Klaus.Hoermann-Engl@world-direct.at>
2023-11-20 10:45:13 +0100

    fix: set correct middleware positioning for v10

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -11,10 +11,10 @@ return [
         'healthcheck' => [
             'target' => \WorldDirect\Healthcheck\Middleware\HealthcheckMiddleware::class,
             'after' => [
-                'typo3/cms-core/verify-host-header'
+                'typo3/cms-frontend/static-route-resolver'
             ],
             'before' => [
-                'typo3/cms-frontend/eid'
+                'typo3/cms-frontend/page-resolver'
             ]
         ]
     ],
