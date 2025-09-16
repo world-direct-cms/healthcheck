e5d77dc427e12f506e9ce2aecd47cacb08d4d461
Klaus HÃrmann-Engl <Klaus.Hoermann-Engl@world-direct.at>
2025-09-16 09:22:24 +0200

    task: remove static TS template loading from ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -2,10 +2,3 @@
 
 defined('TYPO3') || die('Access denied.');
 
-call_user_func(
-    function () {
-        //---------------------------------------------------------------------
-        // Load static TS template
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('healthcheck', 'Configuration/TypoScript', 'healthcheck');
-    }
-);
